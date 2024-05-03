92388867b4ab515d59278b285c75fc9154516c97
SevenDev <14262504+cryptoz0ne@users.noreply.github.com>
2024-05-03 18:26:02 +0900

    use react-bootstrap dropdown and fetch countries and states from staging server (#1)
    Co-authored-by: SevenDev <neolontu@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import { Stack, Dropdown } from 'react-bootstrap';
+import { getCountries, getStates } from '../api';
+
+import './App.css';
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState({})
+  const [states, setStates] = useState([])
+  const [selectedState, setSelectedState] = useState({})
+
+  useEffect(() => {
+    getCountries()
+      .then(response => response.json())
+      .then(data => setCountries(data))
+  }, [])
+
+  useEffect(() => {
+    if(selectedCountry.id) {
+      setSelectedState({})
+      setStates([])
+      
+      getStates(selectedCountry.id)
+        .then(response => response.json())
+        .then(data => setStates(data))
+    }
+  }, [selectedCountry])
+
+  return (
+    <Stack gap={3} className="p-4">
+      <Dropdown>
+        <Dropdown.Toggle variant="secondary" disabled={countries.length === 0}>
+          {selectedCountry.id ? selectedCountry.value : 'Select a country'}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          {countries.map(({id, value}) => (
+            <Dropdown.Item key={`${id}_${value}`} onClick={() => setSelectedCountry({id, value})}>{value}</Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Dropdown>
+        <Dropdown.Toggle variant="secondary" disabled={states.length === 0}>
+          {selectedState.id ? selectedState.value : 'Select a state'}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          {states.map(({id, value}) => (
+            <Dropdown.Item key={`${id}_${value}`} onClick={() => setSelectedState({id, value})}>{value}</Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+    </Stack>
+  );
+}
+
+export default App;
